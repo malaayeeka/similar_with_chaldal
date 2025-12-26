d8e834d07a5d84ca99e149e62110b772b1493223
Malaayeeka Tahseen Khan <tahseenkhan8903@gmail.com>
2025-12-26 19:28:16 +0600

    Added Cards

--- app/page.js
+++ app/page.js
@@ -2,6 +2,8 @@
 import Navbar from "@/components/Navbar/Navbar";
 import Banner from "@/components/Banner/Banner"; 
 import Sidebar from "@/components/Navbar/Sidebar";
+import Cards from "@/components/Cards/Cards";
+// import PopularCategories from "@/components/PopularCategories/PopularCategories";
 
 export default function Home() {
   return (
@@ -9,6 +11,8 @@ export default function Home() {
       <Navbar />
       <Banner />
       <Sidebar />
+      <Cards/>
+      {/* <PopularCategories /> */}
     </>
   );
 }
